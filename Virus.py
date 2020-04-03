d4c218d998d56499e6619903dfa81e1e2f22c613
Daniel Bilodeau <danyboy20009@gmail.com>
2020-04-03 10:49:13 -0400

    integrer fonction Move de Enemy() pas encore fonctionnelle (peut pas visualiser le rectangle)

--- Virus.py
+++ Virus.py
@@ -14,6 +14,7 @@ def main():
     screen = pg.display.set_mode((screen_width, screen_height))
     clock = pg.time.Clock()
     done = False
+    enemies = []
 
     while not done:
         for event in pg.event.get():
@@ -23,17 +24,30 @@ def main():
 
         # Control de tout le clavier
         keys = pg.key.get_pressed()
+        release_p = 0
         if keys[pg.K_a]:  #to move left.
             player1.direction = 2
+
         elif keys[pg.K_d]: #to move right
             player1.direction = 1
         else:
             player1.direction = 0
 
+        if keys[pg.K_p] and release_p == 0:
+            enemies.append(Enemy())
+            print(enemies)
+            release_p = 1
+        elif not keys[pg.K_p] and release_p == 1:
+            release_p = 0
+
         player1.Move()
 
+        if len(enemies) > 0:
+            print('trying to move')
+            enemies[0].Move()
 
-        screen.fill((40, 40, 40))                       #couleur backgroud
+
+        screen.fill((40, 40, 40))                       #couleur backgroud besoin d'être dans la loop?
         pg.draw.rect(screen, (150, 200, 20), player1.drawing)      #??? besoin d'être dans la loop??? c'est quoi
 
         pg.display.flip()                               #Update L'écran au complet
@@ -57,6 +71,26 @@ class Player():
         elif self.direction == 2:
             self.drawing.x -= self.speed
 
+class Enemy():
+    def __init__(self):
+        global screen_width
+        global screen_height
+        rd.seed()
+        self.width = 100
+        self.height = 100
+        self.x_pos = rd.randint(0, screen_width - self.width)
+        self.y_pos = 0-self.height              #crée le bloc au dessus de l'écran quand il spawn
+        self.speed = 5
+        self.drawing = pg.Rect(self.x_pos, self.y_pos, self.width, self.height)
+        #corona = pg.circle(300, 20, 20, 20)
+
+    def Move(self):
+#        pg.draw.circle(screen, (255, 10, 10), (enemy, 20, 20, 20))
+        print("x:", self.x_pos)
+        print("y:", self.y_pos)
+        self.y_pos += self.speed
+        self.drawing.y += self.speed
+
 
 
 if __name__ == '__main__':
@@ -64,20 +98,3 @@ if __name__ == '__main__':
     main()
     pg.quit()
     sys.exit()
-
-
-class Enemy():
-    def __init__(self):
-        global screen_width
-        global screen_height
-        rd.seed()
-        self.enemy = rd.randint(100,540)
-        self.width = 100
-        self.height = 100
-        self.x_pos = rd.randint(0, screen_width - self.width)
-        self.y_pos = 0-self.height              #crée le bloc au dessus de l'écran quand il spawn
-        self.drawing = pg.Rect(self.x_pox, self.y_pox, self.screen_width, self.height)
-        #corona = pg.circle(300, 20, 20, 20)
-
-    def update():
-        pg.draw.circle(screen, (255, 10, 10), (enemy, 20, 20, 20))
