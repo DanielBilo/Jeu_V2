b0e898d24ddca13e868d6b9bd4b335a1a6e0e903
SimonR99 <simonroy99@hotmail.ca>
2020-04-04 19:59:50 -0400

    background

--- Virus.py
+++ Virus.py
@@ -9,6 +9,10 @@ screen_height = 480  #define screen height
 game_speed = 30 #game speed must be changed to make AI learn faster (30 is normal, 100 is faster)
 
 img_virus = pg.image.load('virus_1.png')
+img_bg = pg.image.load('background.png')
+
+#BackGround = Background('background.png', [0,0])
+
 
 def main():
 
@@ -22,6 +26,7 @@ def main():
         enemies.append(Enemy())
 
     while not done:
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 done = True
@@ -36,19 +41,26 @@ def main():
             player1.direction = 1
         else:
             player1.direction = 0
+        if keys[pg.K_p]:
+            exit()
 
         player1.Move()
 
-        screen.fill((40, 40, 40))                       #couleur backgroud besoin d'être dans la loop?
+        screen.fill((0, 150, 255))
+        draw_bg = pg.Rect(0, 0, screen_width, screen_height)                       #couleur backgroud besoin d'être dans la loop?
+        screen.blit(img_bg, draw_bg)
+
         pg.draw.rect(screen, (150, 200, 20), player1.drawing)
+
 
         if len(enemies) > 0:
             for x in range (0,len(enemies) - 1):
                 enemies[x].Move()
+                #pg.draw.rect(screen, (150, 200, 20), enemies[x].drawing)
                 screen.blit(img_virus, enemies[x].drawing)
 
         CheckColision(player1, enemies)
-        pg.display.flip()                               #Update L'écran au complet
+        pg.display.flip()                                       #Update L'écran au complet
         clock.tick(game_speed)                                  #1 frame au 30 millisecondes (delaie l'update de pygame)
 
 class Player():
@@ -58,9 +70,9 @@ class Player():
         self.width = 20
         self.height = 20
         self.x_pos = screen_width/2
-        self.y_pos = screen_height - self.height
+        self.y_pos = screen_height - self.height - 40
         self.drawing = pg.Rect(self.x_pos, self.y_pos, self.width, self.height)
-        self.direction = 0 # 0 = neutre // 1 = gauche // 2 = droite
+        self.direction = 0                                      # 0 = neutre // 1 = gauche // 2 = droite
         self.speed = 4
 
     def Move(self):
